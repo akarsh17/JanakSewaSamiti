e668d0e69717de35377ba23f057f763a08429df2
akarsh17 <akarsh.banthiya2@gmail.com>
2020-09-22 02:05:50 +0530

    eighth commit

--- contactform.php
+++ contactform.php
@@ -1,37 +1,46 @@
 <?php
-if(isset($_POST['sendmail']))
+if(isset($_POST['sendmail'])){
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
+      
+    // if(empty($name) || empty($visitor_email) || empty($subject) || empty($message)){
+    //   header('location:contact.html?error');
+    // }
 
-    $email_from = 'aakarsh.banthiya@gmail.com'
+
+
+
+
+
+    $email_from = "aakarsh.banthiya@gmail.com";
 
     if(isset($message)){
       $email_subject = $subject;
     }
     else{
-      $email_subject = "New Form Submmission from $name";
+      $email_subject = "JanakSewaSamiti - Contact Us Form";
     }
 
-    $email_body = "User Name : $name \n".
-                    "User Email : $visitor_email \n".
-                      "User Message : $message";
+    $email_body = "User : $name \n".
+                    "Email : $visitor_email \n".
+                      "Subject : $subject".
+                        "Message : $message";
 
     $to = 'akarshbanthiya@outlook.com';
 
-    $headers = "From : $email_from";
-    $headers .= "Reply-To: $visitor_email \r\n";
+    $headers = "From: ".$email_from;
 
-    if(mail($to,$email_subject,$email_body,$headers)}{
-      echo"<h1> Sent Successfully! Thank You.".$name.", We will get back to you shortly :) </h1>";
+    if(mail($to,$email_subject,$email_body,$headers)){
+      echo "<h1> Sent Successfully! Thank You.".$name.", We will get back to you shortly :) </h1>";
     }
     else{
-      echo"<h1>Something went Wrong!</h1>";
+      echo "<h1>Something went Wrong!</h1>";
     }
 
+}
 
-
-    header("Location : contact.html");
+    // header("Location : contact.html");
 
 ?>
