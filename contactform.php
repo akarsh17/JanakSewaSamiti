be05bcbae560e5b97d6944cb44c3cd513249b0f8
akarsh17 <akarsh.banthiya2@gmail.com>
2020-09-21 15:42:19 +0530

    seventh commit

--- contactform.php
+++ contactform.php
@@ -1,28 +1,29 @@
 <?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+if(isset['sendmail'])
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $email_from = 'aakarsh.banthiya@gmail.com'
+    $email_from = 'aakarsh.banthiya@gmail.com'
 
-  if(isset($message)){
-    $email_subject = $subject;
-  }
-  else{
-    $email_subject = "New Form Submmission from $name";
-  }
+    if(isset($message)){
+      $email_subject = $subject;
+    }
+    else{
+      $email_subject = "New Form Submmission from $name";
+    }
 
-  $email_body = "User Name : $name \n"
-                  "User Email : $visitor_email \n"
-                    "User Message : $message"
+    $email_body = "User Name : $name \n"
+                    "User Email : $visitor_email \n"
+                      "User Message : $message"
 
-  $to = 'akarshbanthiya@outlook.com';
+    $to = 'akarshbanthiya@outlook.com';
 
-  $headers = "From : $email_from";
-  $headers .= "Reply-To: $visitor_email \r\n";
+    $headers = "From : $email_from";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-  mail($to,$email_subject,$email_body,$headers);
-  header("Location : contact.html");
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location : contact.html");
 
 ?>
