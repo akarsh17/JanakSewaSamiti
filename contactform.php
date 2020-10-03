52b9b5d460cd6d7c9d5e0f7b528045f7d69540bf
akarsh17 <akarsh.banthiya2@gmail.com>
2020-10-04 01:13:32 +0530

    twelfth commit

--- contactform.php
+++ contactform.php
@@ -1,46 +1,98 @@
 <?php
+
+$template_file = "template_file_contactform.php";
+error_reporting(0);
 if(isset($_POST['sendmail'])){
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-      
-    // if(empty($name) || empty($visitor_email) || empty($subject) || empty($message)){
-    //   header('location:contact.html?error');
-    // }
 
 
+  if (empty($_POST["name"])) {
+  $nameErr = "Name is required";
+} else {
+  $name = test_input($_POST["name"]);
+  // check if name only contains letters and whitespace
+  if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+    $nameErr = "Only letters and white space allowed";
+  }
+}
+
+if (empty($_POST["email"])) {
+   $emailErr = "Email is required";
+ } else {
+   $visitor_email = test_input($_POST["email"]);
+   // check if e-mail address is well-formed
+   if (!filter_var($visitor_email, FILTER_VALIDATE_EMAIL)) {
+     $emailErr = "Invalid email format";
+   }
+ }
+
+ if (empty($_POST["subject"])) {
+   $subject = "";
+ } else {
+   $subject = test_input($_POST["subject"]);
+   if (!filter_var($subject,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $subjectErr = "Invalid subject format";}
+ }
 
 
-
+ if (empty($_POST["message"])) {
+   $message = "";
+ } else {
+   $message = test_input($_POST["message"]);
+   if (!filter_var($message,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $subjectErr = "Invalid message format";}
+ }
 
     $email_from = "aakarsh.banthiya@gmail.com";
 
-    if(isset($message)){
-      $email_subject = $subject;
-    }
-    else{
+    if(empty($subject)){
       $email_subject = "JanakSewaSamiti - Contact Us Form";
     }
+    else{
+      $email_subject = $subject;
+    }
 
-    $email_body = "User : $name \n".
-                    "Email : $visitor_email \n".
-                      "Subject : $subject".
-                        "Message : $message";
+    if (file_exists($template_file))
+        $email_body = file_get_contents($template_file);
+    else
+        die("Unable to locate the template file");
+
+
+    // create a list of the variables to be swapped in the html template
+    $swap_var = array(
+      "{SITE_ADDR}" => "https://akarsh17.github.io/JanakSewaSamiti/",
+      "{EMAIL_TITLE}" => "JanakSewaSamiti - Contact Us Form ",
+      "{NAME}" => $name,
+      "{EMAIL}" => $visitor_email,
+      "{SUBJECT}" => $subject,
+      "{MESSAGE}" => $message
+    );
+    foreach (array_keys($swap_var) as $key){
+    if (strlen($key) > 2 && trim($swap_var[$key]) != '')
+      $email_body = str_replace($key, $swap_var[$key], $email_body);
+  }
 
     $to = 'akarshbanthiya@outlook.com';
 
-    $headers = "From: ".$email_from;
+    $headers = "From: JanakSewaSamiti <".$email_from.">\r\nReply-To: ".$name."<".$visitor_email.">\r\n";
+    $headers.="MIME-Version: 1.0\r\n";
+    $headers.= "Content-Type: text/html; charset=ISO-8859-1";
 
     if(mail($to,$email_subject,$email_body,$headers)){
-      echo "<h1> Sent Successfully! Thank You.".$name.", We will get back to you shortly :) </h1>";
+      echo '<script>alert("Message Sent Successfully !\\nThank You !")</script>';
     }
     else{
-      echo "<h1>Something went Wrong!</h1>";
+      echo '<script>alert("Uhhoo, Some error occured.\\nPlease resend the message.")</script>';
     }
-
+    // mail($to,$email_subject,$email_body,$headers);
 }
 
-    // header("Location : contact.html");
 
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+    // header("Refresh:3; contact.html");
+    echo("<script>location.href = '/contact.html';</script>");
 ?>
---
+++ volunteerform.php
@@ -0,0 +1,172 @@
+<?php
+$template_file = "template_file.php";
+$template_file_REPLY = "template_file_REPLY.php";
+
+error_reporting(0);
+if(isset($_POST['sendform'])){
+
+
+  if (empty($_POST["fname"])) {
+  $fnameErr = "Name is required";
+} else {
+  $fname = test_input($_POST["fname"]);
+  // check if name only contains letters and whitespace
+  if (!preg_match("/^[a-zA-Z-' ]*$/",$fname)) {
+    $fnameErr = "Only letters and white space allowed";
+    $fname = '';
+  }
+}
+
+if (empty($_POST["lname"])) {
+$lnameErr = "Name is required";
+} else {
+$lname = test_input($_POST["lname"]);
+// check if name only contains letters and whitespace
+if (!preg_match("/^[a-zA-Z-' ]*$/",$lname)) {
+  $lnameErr = "Only letters and white space allowed";
+  $lname = '';
+}
+}
+
+if (empty($_POST["email"])) {
+   $visitor_emailErr = "Email is required";
+ } else {
+   $visitor_email = test_input($_POST["email"]);
+   // check if e-mail address is well-formed
+   if (!filter_var($visitor_email, FILTER_VALIDATE_EMAIL)) {
+     $visitor_emailErr = "Invalid email format";
+     $visitor_email = '';
+   }
+ }
+
+ if (empty($_POST["phone"])) {
+   $phone = "";
+ } else {
+   $phone = test_input($_POST["phone"]);
+   if (!preg_match('/^[0-9]{10}+$/', $phone)) {
+     $phoneErr = "Invalid subject format";
+     $phone = '';
+   }
+ }
+
+ if (empty($_POST["address"])) {
+   $address = "";
+ } else {
+   $address = test_input($_POST["address"]);
+   if (!filter_var($address,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $addressErr = "Invalid subject format";
+     $address = '';
+   }
+ }
+
+ if (empty($_POST["city"])) {
+   $city = "";
+ } else {
+   $city = test_input($_POST["city"]);
+   if (!filter_var($city,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $cityErr = "Invalid subject format";
+     $city = '';
+   }
+ }
+
+ if (empty($_POST["qualification"])) {
+   $qualification = "";
+ } else {
+   $qualification = test_input($_POST["qualification"]);
+   if (!filter_var($qualification,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $qualificationErr = "Invalid subject format";
+     $qualification = '';
+   }
+ }
+
+ if (empty($_POST["volunteeringfor"])) {
+   $volunteeringfor = "";
+ } else {
+   $volunteeringfor = test_input($_POST["volunteeringfor"]);
+   if (!filter_var($volunteeringfor,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $volunteeringforErr = "Invalid subject format";
+     $volunteeringfor = '';
+   }
+ }
+
+ if (empty($_POST["message"])) {
+   $message = "";
+ } else {
+   $message = test_input($_POST["message"]);
+   if (!filter_var($message,  FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW | FILTER_FLAG_ENCODE_HIGH | FILTER_FLAG_ENCODE_AMP)) {
+     $subjectErr = "Invalid message format";}
+ }
+
+
+    if(empty($subject)){
+      $email_subject = "JanakSewaSamiti -Volunteer Form";
+    }
+    else{
+      $email_subject = $subject;
+    }
+if (file_exists($template_file))
+    $email_body = file_get_contents($template_file);
+else
+    die("Unable to locate the template file");
+
+if (file_exists($template_file_REPLY))
+    $reply_body = file_get_contents($template_file_REPLY);
+else
+    die("Unable to locate the template file");
+
+    // create a list of the variables to be swapped in the html template
+    $swap_var = array(
+      "{SITE_ADDR}" => "https://akarsh17.github.io/JanakSewaSamiti/",
+      "{EMAIL_TITLE}" => "Volunteer Form Details : ",
+      "{NAME}" => $fname." ".$lname,
+      "{EMAIL}" => $visitor_email,
+      "{ADDRESS}" => $address,
+      "{CITY}" => $city,
+      "{QUALIFICATION}" => $qualification,
+      "{VOLUNTEERING_FOR}" => $volunteeringfor,
+      "{MESSAGE}" => $message,
+      "{PHONE}" => $phone,
+      "{REPLY_TITLE}" => "Application Recieved"
+    );
+    foreach (array_keys($swap_var) as $key){
+		if (strlen($key) > 2 && trim($swap_var[$key]) != '')
+			$email_body = str_replace($key, $swap_var[$key], $email_body);
+	}
+
+  foreach (array_keys($swap_var) as $key){
+  if (strlen($key) > 2 && trim($swap_var[$key]) != '')
+    $reply_body = str_replace($key, $swap_var[$key], $reply_body);
+  }
+
+
+    $email_from = "aakarsh.banthiya@gmail.com";
+    $to = 'akarshbanthiya@outlook.com';
+
+    $headers = "From: JanakSewaSamiti<".$email_from.">\r\nReply-To: ".$name."<".$visitor_email.">\r\n";
+    $headers.="MIME-Version: 1.0\r\n";
+    $headers.= "Content-Type: text/html; charset=ISO-8859-1";
+
+
+    if(mail($to,$email_subject,$email_body,$headers)){
+      $headers = "From: JanakSewaSamiti <".$email_from.">\r\nReply-To: JanakSewaSamiti <".$email_from.">\r\n";
+      $headers.="MIME-Version: 1.0\r\n";
+      $headers.= "Content-Type: text/html; charset=ISO-8859-1";
+      mail($visitor_email,$swap_var['{REPLY_TITLE}'],$reply_body,$headers);
+      echo '<script>alert("Volunteer Application Sent Successfully !\\nSomeone will get back to you shortly.")</script>';
+    }
+    else{
+      echo '<script>alert("Uhhoo, Some error occured.\\nPlease resend the Application.")</script>';
+    }
+    // mail($to,$email_subject,$email_body,$headers);
+}
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+    // header("Refresh:3; contact.html");
+    echo("<script>location.href = '/volunteer.html';</script>");
+?>
